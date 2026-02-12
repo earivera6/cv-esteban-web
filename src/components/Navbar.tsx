8d0d174f51c7fd10f676514dacfe7568fb7b6203
earivera6 <estebanrivera200039@gmail.com>
2026-02-12 00:30:15 -0500

    Add structured data (Schema.org Person)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,9 +56,9 @@ export default function Navbar({
   ];
 
   const logoSrc =
-  theme === "dark"
-    ? `${import.meta.env.BASE_URL}brand/logo_esteban_dark.png`
-    : `${import.meta.env.BASE_URL}brand/logo_esteban_light.png`;
+    theme === "dark"
+      ? `${import.meta.env.BASE_URL}brand/logo_esteban_dark.png`
+      : `${import.meta.env.BASE_URL}brand/logo_esteban_light.png`;
 
   return (
     <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
